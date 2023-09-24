6d823a0b4dcbcb4a42e7fc4e329edfc0aee95930
Xusrav <xusrav200329@gmail.com>
2023-09-24 17:30:09 +0500

    Post yaratish uchun View qoshdim va CKEditorni ulab qoydim

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreate
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
-
+    path('post_create/', PostCreate.as_view(), name='create'),
 ]
